4980f0acbf125779256499e8858d4b16507adce4
Luis.nfz <lfgonzalezvergara@estudiante.uniajc.edu.co>
2025-05-13 14:27:53 +0000

    clase sistema Autenticaciones mejorada

--- biblioteca/src/main/java/com/biblioteca/SistemaAutenticacion.java
+++ biblioteca/src/main/java/com/biblioteca/SistemaAutenticacion.java
@@ -6,7 +6,7 @@ public class SistemaAutenticacion {
     private List<Usuario> usuariosRegistrados;
 
     public SistemaAutenticacion() {
-        // Constructor vacío
+    
     }
 
     public SistemaAutenticacion(List<Usuario> usuariosRegistrados) {
@@ -14,20 +14,20 @@ public class SistemaAutenticacion {
     }
 
     public void login(String correo, String contraseña) {
-        // lógica para login
+    
     }
 
     public void logout() {
-        // lógica para cerrar sesión
+    
     }
 
     public boolean verificarCredenciales(String correo, String contraseña) {
-        // Aquí deberías buscar en la lista si hay un usuario con ese correo y contraseña
+    
         return usuariosRegistrados.stream()
                 .anyMatch(u -> u.getCorreo().equals(correo) && u.getContraseña().equals(contraseña));
     }
 
-    // Getters y Setters
+    
     public List<Usuario> getUsuariosRegistrados() {
         return usuariosRegistrados;
     }
